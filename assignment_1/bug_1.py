19c37179e147c2004b2c869417d5019692c3f00d
nakulrandad <nakulrandad@gmail.com>
2022-02-08 17:58:08 +0530

    bug_1 modified

--- assignment_1/bug_1.py
+++ assignment_1/bug_1.py
@@ -49,6 +49,49 @@ result.pose_final.y = start[1]
 result.pose_final.theta = 0  # in radians (0 to 2pi)
 current_pose = np.array([result.pose_final.x, result.pose_final.y])
 path = [current_pose]
+# is_near_obs = False
+
+
+def update_waypoint(dir_vec, wp):
+    wp.pose_dest.x = result.pose_final.x + dir_vec[0] * step_size
+    wp.pose_dest.y = result.pose_final.y + dir_vec[1] * step_size
+    # theta is the orientation of robot in radians (0 to 2pi)
+    wp.pose_dest.theta = math.atan2(dir_vec[1], dir_vec[0])
+    # send waypoint to turtlebot3 via move_xy server
+    client.send_goal(wp)
+    client.wait_for_result()
+    # getting updated robot location
+    result = client.get_result()
+    current_pose = np.array([result.pose_final.x, result.pose_final.y])
+
+
+def move_to_goal(current_pose):
+    dir_vec = (goal - current_pose) / np.linalg.norm(goal - current_pose)
+    update_waypoint(dir_vec, MoveXYGoal())
+    
+    
+def circumnavigate_obs(obstacle, init_pose):
+    dir_vec = -np.array(computeTangentVectorToPolygon(obstacle, init_pose))
+    dist = float("inf")
+    while True:
+        update_waypoint(dir_vec, MoveXYGoal())
+        dir_vec = -np.array(computeTangentVectorToPolygon(obstacle, current_pose))
+        if distance(current_pose, goal) < dist:
+            obs_leave_node = current_pose
+            dist = distance(current_pose, goal)
+        if distance(current_pose, init_pose) < step_size:
+            break
+    return obs_leave_node
+
+
+def depart_obs(obstacle, obs_leave_node)
+    dir_vec = -np.array(computeTangentVectorToPolygon(obstacle, current_pose))
+    while True:
+        update_waypoint(dir_vec, MoveXYGoal())
+        dir_vec = -np.array(computeTangentVectorToPolygon(obstacle, current_pose))
+        if distance(current_pose, obs_leave_node) < step_size:
+            break
+
 
 while distance(current_pose, goal) > step_size:
     dist = float("inf")
@@ -60,32 +103,16 @@ while distance(current_pose, goal) > step_size:
 
     # print(f"Distance to obs {i}: {dist}")
 
-    dir_vec = (goal - current_pose) / np.linalg.norm(goal - current_pose)
-
     dist_to_polygon_frd = computeDistancePointToPolygon(
         obstacles[closest_obs_id], current_pose + dir_vec * step_size * 0.8
     )
     if dist_to_polygon_frd < step_size:
+        init_pose = current_pose
         dir_vec = -np.array(
             computeTangentVectorToPolygon(
                 obstacles[closest_obs_id], current_pose
             )
         )
-
-    wp = MoveXYGoal()
-    wp.pose_dest.x = result.pose_final.x + dir_vec[0] * step_size
-    wp.pose_dest.y = result.pose_final.y + dir_vec[1] * step_size
-    # theta is the orientation of robot in radians (0 to 2pi)
-    wp.pose_dest.theta = math.atan2(dir_vec[1], dir_vec[0])
-
-    # send waypoint to turtlebot3 via move_xy server
-    client.send_goal(wp)
-
-    client.wait_for_result()
-
-    # getting updated robot location
-    result = client.get_result()
-    current_pose = np.array([result.pose_final.x, result.pose_final.y])
 
     # write to output file (replacing the part below)
     path.append(current_pose)
